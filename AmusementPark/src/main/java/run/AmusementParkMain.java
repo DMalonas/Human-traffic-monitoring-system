2aec889ae5fbeebf9977003cf02ad4f876ad7f17
DimitriosMalonas <dimitriosmalonas@outlook.com>
2020-01-06 04:54:44 +0000

    Added MessageTypes class in utilities package. Class contains all possible system messages declared as final variables

--- AmusementPark/src/main/java/run/AmusementParkMain.java
+++ AmusementPark/src/main/java/run/AmusementParkMain.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 
 import nodes.ParkNode;
 import nodes.ParkNodesInputType;
+import utilities.MessageTypes;
 
 public class AmusementParkMain {
 	public static void main(String[] args) {
@@ -17,9 +18,10 @@ public class AmusementParkMain {
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
 			String input = scanner.nextLine();
-			if (input.equals("ELECTION") || input.equals("LEAVE"))
+			if (input.equals(MessageTypes.ENTER_MESSAGE) || input.equals(MessageTypes.LEAVE_MESSAGE)) {
 				System.out.println("input is " + input);
 				parkNode.receiveFromConsole(input);
+			}
 		}
 	}
 }
---
+++ AmusementPark/src/main/java/utilities/MessageTypes.java
@@ -0,0 +1,10 @@
+package utilities;
+
+public class MessageTypes {
+	
+	public static final String ENTER_MESSAGE = "ENTER";
+	
+	
+	public static final String LEAVE_MESSAGE = "LEAVE";
+
+}
